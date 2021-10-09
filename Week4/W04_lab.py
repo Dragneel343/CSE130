b43a739b9bdc50c0737764be94a177040686d2c3
Dragneel343 <37350944+Dragneel343@users.noreply.github.com>
2021-10-09 13:41:35 -0600

    finish Base coding

--- Week4/W04_lab.py
+++ Week4/W04_lab.py
@@ -16,62 +16,131 @@ def main():
     end_turn = 0
 
     #loop through the prompts until we get a hotel.
-    while hotel != 1 | end_turn == 0:
-
-        #user input for all green property.
-        property = str(input("Do you own all the green properties? (Y/N)"))
+    while hotel != 1 or end_turn == 0:
+        
+        #prompts user to see if they own all the proptery.
+        property = input("Do you own all the green properties? (y/n)\n: ")
 
         #Checks response for output.
-        if property == "N":
+        if property == "n":
             print("You cannot purcahse a hotel until you own all the properties of a given color group.")
             end_turn = 1
-        elif property != "Y" | property != "N":
+            return end_turn
+        #elif property != "y" or property != "n":
             print("Please enter the proper response.")
-            property = str(input("Do you own all the green properties? (Y/N)"))
+            property = str(input("Do you own all the green properties? (y/n)\n:"))
+
         
-        #user input for Pennsylvania Avenue.
-        penn = int(input("What is on Pennsylvania Avenue? (0:nothing, 1:one house, 2:two houses, 3:three houses, 4:four houses, or 5:a hotel"))
+        #prompts user for what is on Pennsylvania.
+        penn = int(input("What is on Pennsylvania Avenue? (0:nothing, 1:one house, 2:two houses, 3:three houses, 4:four houses, or 5:a hotel)\n: "))
 
         #check response for PA output.
         if penn == 5:
             print("You cannot purcahse a hotel if the property already has one.")
             hotel = 1
-        elif penn < 5 | penn > 0:
+            return hotel
+        #elif penn > 5 or penn < 0:
             print("Please enter the proper response.")
 
-        #user input for North Carolina.
-        carolina = int(input("What is on North Carolina Avenue? (0:nothing, 1:one house, 2:two houses, 3:three houses, 4:four houses, or 5:a hotel"))
+        #prompts user for what is on North Carolina.
+        carolina = int(input("What is on North Carolina Avenue? (0:nothing, 1:one house, 2:two houses, 3:three houses, 4:four houses, or 5:a hotel)\n: "))
 
         #checks response for NC output.
         if carolina == 5:
             print("Swap North Carolina's hotel for Pennsylvania's 4 houses.")
             end_turn = 1
-        elif carolina < 5 | carolina > 0:
+            return end_turn
+        #elif carolina > 5 or carolina < 0:
             print("Please enter the proper response.")
             carolina = int(input("What is on North Carolina Avenue? (0:nothing, 1:one house, 2:two houses, 3:three houses, 4:four houses, or 5:a hotel"))
             
-        #user input for Pacific Avenue.
-        pacific = int(input("What is on Pacific Avenue? (0:nothing, 1:one house, 2:two houses, 3:three houses, 4:four houses, or 5:a hotel"))
+        #prompts user for what is on Pacific.
+        pacific = int(input("What is on Pacific Avenue? (0:nothing, 1:one house, 2:two houses, 3:three houses, 4:four houses, or 5:a hotel)\n:"))
 
         #checks response for PC output.
         if pacific == 5:
             print("Swap Pacific's hotel for Pennsylvania's 4 houses.")
             end_turn = 1
-        elif pacific < 5 | pacific > 0:
+            return end_turn
+        #elif pacific > 5 or pacific < 0:
             print("Please enter the proper response.")
             pacific = int(input("What is on Pacific Avenue? (0:nothing, 1:one house, 2:two houses, 3:three houses, 4:four houses, or 5:a hotel"))
 
-        #user input for available hotels
-        buy_hotels = int(input("How many hotels are there to purchase?"))
+        #prompts user for available hotels.
+        buy_hotels = int(input("How many hotels are there to purchase?\n:"))
 
         #checks response for No Hotels output.
         if buy_hotels == 0:
             print("There are not enough hotels available for purchase at this time.")
             end_turn = 1
-        elif buy_hotels > 0 | buy_hotels < 12:
+            return end_turn
+        #elif buy_hotels < 0 or buy_hotels > 12:
             print("Please enter the proper response.")
             buy_hotels = int(input("How many hotels are there to purchase?"))
+        
+        
+        #Math for calculated houses need for each property.
+        PA_need = 4 - penn
+        NC_need = 4 - carolina
+        PC_need = 4 - pacific
 
+        #money needed per property.
+        NC_money_need = NC_need * 200
+        PC_money_need = PC_need * 200
+
+        #Math for total houses needed for a hotel.
+        total_need = PA_need + NC_need + PC_need
+
+        #Math for money needed to build all the houses. The extra 200 at the end is for buying one hotel.
+        total_money_need = (200 * total_need) + 200
+
+        #prompts user for total cash available.
+        cash = int(input("How much money do you have to spend?\n:"))
+
+        #checks response for cash output.
+        if cash < total_money_need:
+            print("You do not have suffiecient funds to purchase a hotel at this time")
+            end_turn = 1
+            return end_turn
+
+        #prompts user for available Houses.
+        #HEY ANDREW PUT THE ERROR HANDLING HERE AROUND THE ORIGINAL INPUT!!!
+        houses = int(input("How many house are there to purchase?\n:"))
+
+        #check response for cash houses output.
+        if houses < total_need:
+            print("There are not enough houses available for purchase this time.")
+            end_turn = 1
+            return end_turn
+
+        #Display the amount of houses need to build a hotel.
+        print("The total amount of house need is",total_need,".")
+
+        #Logic for purchasing houses and a hotel.
+        if NC_need > 0:
+            if PC_need > 0:
+                print("This will cost $",total_money_need,".")
+                print("\tPurchase 1 hotel and ",total_need," house(s).")
+                print("\tPut 1 hotel on Pennsylvania and return any houses to the bank.")
+                print("\tPut ",NC_need," house(s) on North Carolina.")
+                print("\tPut ",PC_need," house(s) on Pacific.")
+                hotel = 1
+                return hotel
+            else:
+                print("This will cost $",(NC_money_need + 200),".")
+                print("\tPurchase 1 hotel and ",NC_need," house(s).")
+                print("\tPut 1 hotel on Pennsylvania and return any houses to the bank.")
+                print("\tPut ",NC_need," house(s) on North Carolina.")
+                hotel = 1
+                return hotel
+        else:
+            if PC_need > 0:
+                print("This will cost $",(PC_money_need + 200),".")
+                print("\tPurchase 1 hotel and ",PC_need," house(s).")
+                print("\tPut 1 hotel on Pennsylvania and return any houses to the bank.")
+                print("\tPut ",PC_need," house(s) on Pacific.")
+                hotel = 1
+                return hotel       
 
 if __name__ == "__main__":
-    main()        
+    main()     
